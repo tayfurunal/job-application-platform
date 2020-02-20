ee5f72b77d0644febb0bfcc5dbac2e7b53ddc8da
tayfurunal <mtayfurunal@gmail.com>
2020-02-20 23:29:58 +0300

    spring boot project started

---
+++ hr-application-backend/src/main/java/com/tayfurunal/hrapplication/HrapplicationApplication.java
@@ -0,0 +1,13 @@
+package com.tayfurunal.hrapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrapplicationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HrapplicationApplication.class, args);
+    }
+
+}
---
+++ hr-application-backend/src/test/java/com/tayfurunal/hrapplication/HrapplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tayfurunal.hrapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrapplicationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
