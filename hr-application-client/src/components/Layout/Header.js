554928636f2c2f9c28f9740affdfb38c6de17d3b
tayfurunal <mtayfurunal@gmail.com>
2020-02-23 13:50:45 +0300

    landing and header component

---
+++ hr-application-client/src/components/Layout/Header.js
@@ -0,0 +1,127 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { logout } from '../../../actions/securityActions';
+
+class Header extends Component {
+  logout = () => {
+    this.props.logout();
+    window.location.href = '/';
+  };
+
+  render() {
+    const { validToken, user, roles } = this.props.security;
+
+    const userIsHR = (
+      <div className='collapse navbar-collapse' id='mobile-nav'>
+        <ul className='navbar-nav mr-auto'>
+          <li className='nav-item'>
+            <Link className='nav-link' to='/managerPanel'>
+              Manager Panel
+            </Link>
+          </li>
+        </ul>
+
+        <ul className='navbar-nav ml-auto'>
+          <li className='nav-item'>
+            <Link className='nav-link' to='/dashboard'>
+              <i className='fas fa-user-circle mr-1' />
+              {user.fullName}
+            </Link>
+          </li>
+          <li className='nav-item'>
+            <Link className='nav-link' to='/logout' onClick={this.logout}>
+              Logout
+            </Link>
+          </li>
+        </ul>
+      </div>
+    );
+
+    const userIsAPPLICANT = (
+      <div className='collapse navbar-collapse' id='mobile-nav'>
+        <ul className='navbar-nav mr-auto'>
+          <li className='nav-item'>
+            <Link className='nav-link' to='/'>
+              Ana Sayfa
+            </Link>
+          </li>
+        </ul>
+
+        <ul className='navbar-nav ml-auto'>
+          <li className='nav-item'>
+            <Link className='nav-link' to='/dashboard'>
+              <i className='fas fa-user-circle mr-1' />
+              {user.fullName}
+            </Link>
+          </li>
+          <li className='nav-item'>
+            <Link className='nav-link' to='/logout' onClick={this.logout}>
+              Logout
+            </Link>
+          </li>
+        </ul>
+      </div>
+    );
+
+    const userIsNotAuthenticated = (
+      <div className='collapse navbar-collapse' id='mobile-nav'>
+        <ul className='navbar-nav ml-auto'>
+          <li className='nav-item'>
+            <Link className='nav-link' to='/register'>
+              Sign Up
+            </Link>
+          </li>
+          <li className='nav-item'>
+            <Link className='nav-link' to='/login'>
+              Login
+            </Link>
+          </li>
+        </ul>
+      </div>
+    );
+
+    let headerLinks;
+
+    if (validToken && user) {
+      if (roles[0].includes('ROLE_HR')) {
+        headerLinks = userIsHR;
+      } else {
+        headerLinks = userIsAPPLICANT;
+      }
+    } else {
+      headerLinks = userIsNotAuthenticated;
+    }
+
+    return (
+      <nav className='navbar navbar-expand-sm navbar-dark bg-primary mb-4'>
+        <div className='container'>
+          <Link className='navbar-brand' to='/'>
+            Job Application Platform
+          </Link>
+          <button
+            className='navbar-toggler'
+            type='button'
+            data-toggle='collapse'
+            data-target='#mobile-nav'
+          >
+            <span className='navbar-toggler-icon' />
+          </button>
+          {headerLinks}
+        </div>
+      </nav>
+    );
+  }
+}
+
+Header.propTypes = {
+  logout: PropTypes.func.isRequired,
+  security: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+  security: state.security
+});
+
+export default connect(mapStateToProps, { logout })(Header);
---
+++ hr-application-client/src/components/Layout/Landing.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { getJobs } from '../../../actions/jobActions';
+import ProjectItem from '../ProjectItem';
+
+class Landing extends Component {
+  componentDidMount() {
+    this.props.getJobs();
+  }
+
+  render() {
+    const jobs = this.props.job.jobs;
+
+    return (
+      <div className='projects'>
+        <div className='container'>
+          <div className='row'>
+            <div className='col-md-12'>
+              <h1 className='display-4 text-center'>Available Jobs</h1>
+              <hr />
+              {jobs.map(job => (
+                <ProjectItem key={job.id} job={job} />
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+Landing.propTypes = {
+  getJobs: PropTypes.func.isRequired,
+  job: PropTypes.object.isRequired,
+  security: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+  job: state.job,
+  security: state.security
+});
+
+export default connect(mapStateToProps, { getJobs })(Landing);
