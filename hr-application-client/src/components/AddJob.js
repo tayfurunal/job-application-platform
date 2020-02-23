ab9675d61499c88dc97b6b932a3315acf1a37420
tayfurunal <mtayfurunal@gmail.com>
2020-02-23 17:21:31 +0300

    AddJob Component Route fix

--- hr-application-client/src/components/AddJob.js
+++ hr-application-client/src/components/AddJob.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { createJob } from '../../actions/jobActions';
+import { createJob } from '../actions/jobActions';
 
 class AddJob extends Component {
   constructor() {
