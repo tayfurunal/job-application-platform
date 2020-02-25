3dd41d7f576d6a90dbb37d8ab90b503aecc4711e
tayfurunal <mtayfurunal@gmail.com>
2020-02-25 22:01:35 +0300

    new sql command added with data jpa

--- hr-application-backend/src/main/java/com/tayfurunal/hrapplication/repository/ApplicationRepository.java
+++ hr-application-backend/src/main/java/com/tayfurunal/hrapplication/repository/ApplicationRepository.java
@@ -4,5 +4,8 @@ import com.tayfurunal.hrapplication.domain.Application;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ApplicationRepository extends JpaRepository<Application, Long> {
+    List<Application> findAllByUserUsername(String username);
 }
--- hr-application-backend/src/main/java/com/tayfurunal/hrapplication/repository/JobRepository.java
+++ hr-application-backend/src/main/java/com/tayfurunal/hrapplication/repository/JobRepository.java
@@ -2,8 +2,13 @@ package com.tayfurunal.hrapplication.repository;
 
 import com.tayfurunal.hrapplication.domain.Job;
 
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
 
 public interface JobRepository extends JpaRepository<Job, Long> {
     Job getById(Long id);
+    List<Job> findAllByOrderByIdAsc();
+    List<Job> findAllByIsClosedEqualsOrderByIdAsc(Integer isClosed);
 }
